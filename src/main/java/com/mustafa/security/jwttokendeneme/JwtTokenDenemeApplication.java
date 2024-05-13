da2a941a1745a4364844f45229b7ac52148fa7f5
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-05-13 23:29:50 +0300

    update

--- src/main/java/com/mustafa/security/jwttokendeneme/JwtTokenDenemeApplication.java
+++ src/main/java/com/mustafa/security/jwttokendeneme/JwtTokenDenemeApplication.java
@@ -1,13 +1,13 @@
 package com.mustafa.security.jwttokendeneme;
 
+import com.mustafa.security.jwttokendeneme.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class JwtTokenDenemeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JwtTokenDenemeApplication.class, args);
 	}
-
 }
